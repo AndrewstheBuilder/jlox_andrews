01beebedcbc531cf64ab52ff162e4562c33674a3
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2024-01-04 11:01:45 -0500

    Implement accept method across all of the subclasses of Expr and you should be done with chapter 5

--- com/craftinginterpreters/lox/Expr.java
+++ com/craftinginterpreters/lox/Expr.java
@@ -3,38 +3,54 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
-  static class Binary extends Expr {
-    Binary(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+    abstract void accept(Vistor<String> object);
+
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
     }
 
-    final Expr left;
-    final Token operator;
-    final Expr right;
-  }
-  static class Grouping extends Expr {
-    Grouping(Expr expression) {
-      this.expression = expression;
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        final Expr expression;
     }
 
-    final Expr expression;
-  }
-  static class Literal extends Expr {
-    Literal(Object value) {
-      this.value = value;
+    static class Literal extends Expr {
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        final Object value;
     }
 
-    final Object value;
-  }
-  static class Unary extends Expr {
-    Unary(Token operator, Expr right) {
-      this.operator = operator;
-      this.right = right;
+    static class Unary extends Expr {
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Token operator;
+        final Expr right;
     }
 
-    final Token operator;
-    final Expr right;
-  }
+    public interface Vistor<T> {
+        public T visitBinaryExpr(Expr.Binary expr);
+
+        public T visitGroupingExpr(Expr.Grouping expr);
+
+        public T visitLiteralExpr(Expr.Literal expr);
+
+        public T visitUnaryExpr(Expr.Unary expr);
+    }
+
 }

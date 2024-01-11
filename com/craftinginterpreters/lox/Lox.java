01219ad9f466ae7e46c88d8e816c642449dc0354
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2024-01-10 21:16:35 -0500

    Finish Chapter 6 Parser

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -48,12 +48,20 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
 
     // For now, just print the tokens.
-    System.out.println("-------------------------");
-    System.out.println("type|lexeme|literal");
-    System.out.println("-------------------------");
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    // System.out.println("-------------------------");
+    // System.out.println("type|lexeme|literal");
+    // System.out.println("-------------------------");
+    // for (Token token : tokens) {
+    //   System.out.println(token);
+    // }
+
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    // Stop if there was a syntax error
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
   }
 
   static void error(int line, String message) {
@@ -66,4 +74,12 @@ public class Lox {
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
+
+  static void error (Token token, String message) {
+    if (token.type == TokenType.EOF) {
+        report(token.line, " at end", message);
+    } else {
+        report(token.line, " at '" + token.lexeme + "'", message);
+    }
+}
 }
